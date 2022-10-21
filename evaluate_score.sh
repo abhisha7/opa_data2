a9532fed74f304d905760b0d40a13ba5f8a68d33
Abhishek Sharma <abhsharm23@publicisgroupe.net>
2022-10-21 12:11:54 +0530

    changes

--- evaluate_score.sh
+++ evaluate_score.sh
@@ -17,41 +17,29 @@ autoscaling=$(./opa eval --format pretty --data autoscaling.rego --input tfplan.
 autoscaling="autoscaling:${autoscaling}"
 under_limit=$(./opa eval --format pretty --data terraform.rego --input tfplan.json "data.terraform.analysis.authz")
 
-declare -A score_arr
-score_arr=( "autoscaling:30"
-            "s3:10"
-            "ec2:10"
-            "eip:10"
-            "sg:20" )
-
+declare -A score_arr=([autoscaling]=30 [s3]=10 [ec2]=10 [eip]=10 [sg]=20)
 
 if [[ ${under_limit} == 'true' ]]
 then
     if [[ ${total} > 1 ]]
     then
         all_resources_arr=($eip $sg $ec2 $s3 $autoscaling)
-        #echo ${all_resources_arr[*]}
         final_score=0
         for resource in "${all_resources_arr[@]}"
         do
           item=$(echo $resource | awk -F":" '{print $1}')
           score=$(echo $resource | awk -F":" '{print $2}')
           echo $item
-          present=$(echo ${score_arr[@]} | grep -o ${item})
+          present=$(echo "${!score_arr[@]}" | sed 's/ /\n/g' | grep -o ${item})
           if [ ! -z ${present} ]
           then
-            value=$(echo ${score_arr[@]} | sed 's/ /\n/g' | grep -i ec2 | awk -F":" '{print $2}')
-            if [ $item == $present ]
+            value=$(for i in "${!score_arr[@]}"; do echo "$i":"${score_arr[$i]}"; done | grep -i $item | awk -F":" '{print $2}')
+            if [ $score > 0 ]
             then
-                if [ $score > 0 ]
-                then
-                    final_score=${final_score}+${value}
-                    echo $final_score
-                fi
+                final_score=${final_score}+${value}
+                echo $final_score
             fi
           fi      
         done  
     fi
 fi    
-
-echo $final_score
