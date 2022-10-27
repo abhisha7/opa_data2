7789bae09483ddeede33080bb8a3d0c9963dff4d
Abhishek Sharma <abhsharm23@publicisgroupe.net>
2022-10-27 14:04:09 +0530

    changes

--- evaluate_score.sh
+++ evaluate_score.sh
@@ -11,19 +11,12 @@ terraform plan --out tfplan.binary
 terraform show -json tfplan.binary > tfplan.json
 
 total=$(./opa eval --format pretty --data terraform.rego --input tfplan.json "data.terraform.analysis.score")
-# echo $total
 eip=$(./opa eval --format pretty --data elasticip.rego --input tfplan.json "data.terraform.analysis.score")
-# echo $eip
 sg=$(./opa eval --format pretty --data securitygroup.rego --input tfplan.json "data.terraform.analysis.score")
-# echo $sg
 ec2=$(./opa eval --format pretty --data ec2.rego --input tfplan.json "data.terraform.analysis.score")
-# echo $ec2
 s3=$(./opa eval --format pretty --data s3.rego --input tfplan.json "data.terraform.analysis.score")
-# echo $s3
 autoscaling=$(./opa eval --format pretty --data autoscaling.rego --input tfplan.json "data.terraform.analysis.score")
-# echo $autoscaling
 under_limit=$(./opa eval --format pretty --data terraform.rego --input tfplan.json "data.terraform.analysis.authz")
-# echo $under_limit
 eip="eip:${eip}"
 sg="sg:${sg}"
 ec2="ec2:${ec2}"
@@ -34,14 +27,11 @@ all_resources_arr=()
 for res in "${res_over_zero[@]}"
 do
   res_score=$(echo $res | awk -F":" '{print $2}')
-#   echo $res_score
   if [ $res_score != 0 ]
   then
     all_resources_arr+=($res)
-    # echo ${all_resources_arr[@]}    
   fi
 done
-# echo ${all_resources_arr[@]}
 declare -A score_arr=([autoscaling]=30 [s3]=10 [ec2]=10 [eip]=10 [sg]=20)
 
 if [[ ${under_limit} == 'true' ]]
@@ -57,11 +47,7 @@ then
           if [ ! -z ${present} ]
           then
             value=$(for i in "${!score_arr[@]}"; do echo "$i":"${score_arr[$i]}"; done | grep -i $item | awk -F":" '{print $2}')
-            # if [ $score > 0 ]
-            # then
             final_score=$((final_score + value))
-                
-            # fi
           fi      
         done  
     fi
@@ -81,5 +67,4 @@ then
 else
     echo "Sorry! you failed the test"
 fi    
-#echo $final_score
 cleanup_terraform
