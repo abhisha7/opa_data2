bb6b32a6eccc8c2ca07480a927620b6682ec0b99
Abhishek Sharma <abhsharm23@publicisgroupe.net>
2022-10-27 13:46:34 +0530

    changes

--- main.tf
+++ main.tf
@@ -7,27 +7,27 @@ provider "aws" {
 #  assume_role_policy = "${file("assumerolepolicy.json")}"
 #}
 
-resource "aws_instance" "web" {
- instance_type = "t2.micro"
- ami = "ami-09b4b74c"
+# resource "aws_instance" "web" {
+#  instance_type = "t2.micro"
+#  ami = "ami-09b4b74c"
+# # }
 # }
-}
-resource "aws_autoscaling_group" "my_asg" {
-  availability_zones        = ["us-west-1a"]
-  name                      = "my_asg"
-  max_size                  = 5
-  min_size                  = 1
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
-  desired_capacity          = 4
-  force_delete              = true
-  launch_configuration      = "my_web_config"
-}
-resource "aws_launch_configuration" "my_web_config" {
-    name = "my_web_config"
-    image_id = "ami-09b4b74c"
-    instance_type = "t2.micro"
-}
+# resource "aws_autoscaling_group" "my_asg" {
+#   availability_zones        = ["us-west-1a"]
+#   name                      = "my_asg"
+#   max_size                  = 5
+#   min_size                  = 1
+#   health_check_grace_period = 300
+#   health_check_type         = "ELB"
+#   desired_capacity          = 4
+#   force_delete              = true
+#   launch_configuration      = "my_web_config"
+# }
+# resource "aws_launch_configuration" "my_web_config" {
+#     name = "my_web_config"
+#     image_id = "ami-09b4b74c"
+#     instance_type = "t2.micro"
+# }
 # resource "aws_autoscaling_group" "my_asg2" {
 #   availability_zones        = ["us-west-2a"]
 #   name                      = "my_asg2"
@@ -57,10 +57,10 @@ resource "aws_s3_bucket" "demos3" {
     acl = "private"   
 }
 
-resource "aws_eip" "lb" {
-  instance = aws_instance.web.id
-  vpc      = true
-}
+# resource "aws_eip" "lb" {
+#   instance = aws_instance.web.id
+#   vpc      = true
+# }
 
 resource "aws_security_group" "demo-sg" {
   name = "sec-grp"
